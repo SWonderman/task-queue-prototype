328241fc9b32aaa44b882ff76ee61123913c7115
Szymon Witt <szymonwitt2707@gmail.com>
2024-08-20 12:09:13 +0200

    feat: toggle all order checkboxes using the root checkbox

--- queueproto/static/js/script.js
+++ queueproto/static/js/script.js
@@ -1,1 +1,14 @@
-console.log("Script file is up and runnnig");
+function toggleAllOrders() {
+  const rootCheckbox = document.getElementById("root-checkbox");
+  const orderCheckboxes = document.getElementsByName("order");
+
+  if (rootCheckbox.checked === true) {
+    Array.from(orderCheckboxes).map((checkbox) => {
+      checkbox.checked = true;
+    });
+  } else {
+    Array.from(orderCheckboxes).map((checkbox) => {
+      checkbox.checked = false;
+    });
+  }
+}

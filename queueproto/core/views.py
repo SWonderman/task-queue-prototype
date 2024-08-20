6aabd5c7e943ffc7f2472ed58b2f9c6641bea313
Szymon Witt <szymonwitt2707@gmail.com>
2024-08-20 12:25:18 +0200

    feat: paginated orders list view function

--- queueproto/core/views.py
+++ queueproto/core/views.py
@@ -1,13 +1,18 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator
 from django.db.models import QuerySet
 
 from core.models import Order
 
 def index(request):
     orders: QuerySet[Order] = Order.objects.all()
+    paginator = Paginator(orders, 15)
+
+    page_number = request.GET.get("page")
+    page_orders = paginator.get_page(page_number)
 
     context = {
-        "orders": orders,
+        "orders": page_orders,
     }
 
     return render(request=request, template_name="core/index.html", context=context)

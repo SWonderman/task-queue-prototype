a2f00ee9a022d4888aaffda3daf9a142a0c8bf17
Szymon Witt <szymonwitt2707@gmail.com>
2024-08-21 16:43:22 +0200

    chore: register core models to admin panel

--- queueproto/core/admin.py
+++ queueproto/core/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from core.models import Customer, OrderItem, OrderShipment, OrderHandlingProcess, Order
+
 # Register your models here.
+admin.site.register(Order)
+admin.site.register(OrderItem)
+admin.site.register(OrderShipment)
+admin.site.register(OrderHandlingProcess)
+admin.site.register(Customer)
